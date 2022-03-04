a6c7752e3d13a0e71d66b276c8fb9c00f872b4c2
Alberto Villarreal <alberto.villarreal@keysight.com>
2022-03-04 14:39:48 -0600

    Updates to setup and added pyproject.toml

--- setup.py
+++ setup.py
@@ -7,14 +7,22 @@ def main():
 
     setup(
         name='ixload_py',
+        keywords='IxLoad test automation',
         description='Python wrapper for IxLoad automation',
         author='Alberto Villarreal',
         author_email='alberto.villarreal@keysight.com',
-
+        url='https://github.com/albertovillarreal-keys/ixload_py',
         version='1.0',
         install_requires=requirements,
         packages=find_packages(where='Utils'),
-        package_dir={'': 'Utils'}
+        package_dir={'': 'Utils'},
+        classifiers=[
+                    'Programming Language :: Python :: 3.7',
+                    'Programming Language :: Python :: 3.8',
+                    'Intended Audience :: Developers',
+                    'License :: OSI Approved :: MIT License',
+                    'Operating System :: OS Independent',
+                ]
     )
 
 
